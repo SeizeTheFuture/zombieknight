65a747bb29b2c72a71f9051e25e3b76b6cc2678d
Matt Gottilla <matthew.gottilla@gmail.com>
2022-03-25 21:21:21 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,219 @@
+import pygame, random, sys
+
+#Use 2D vectors
+vector = pygame.math.Vector2
+
+#Initialize pygame
+pygame.init()
+
+#Set the display surface (tile size is 32x32 so 1280/32 = 40 tiles wide, 736/32 = 23 tiles high
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 736
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Zombie Knight")
+
+#Set the FPS and clock
+FPS = 60
+clock = pygame.time.Clock()
+tan = random.randint(1,5)
+
+#Define Classes
+class Game():
+    """A class to manage gameplay"""
+
+    def __init__(self):
+        """Initialize the game"""
+        pass
+
+    def update(self):
+        """Update the game"""
+        pass
+
+    def draw(self):
+        """Draw the game HUD"""
+        pass
+
+    def add_zombie(self):
+        """Add a zombie to the game"""
+        pass
+
+    def check_collisions(self):
+        """Check collisions that affect gameplay"""
+        pass
+
+    def check_round_completion(self):
+        """Check if the player survived a single night"""
+        pass
+
+    def check_game_over(self):
+        """Check to see if the player lost the game"""
+        pass
+
+    def start_new_round(self):
+        """Start a new night"""
+        pass
+
+    def pause_game(self):
+        """Pause the game"""
+        pass
+
+    def reset_game(self):
+        "Reset the game"
+        pass
+
+class Tile(pygame.sprite.Sprite):
+    """A class to represent a 32x32 pixel area in our display"""
+
+    def __init__(self):
+        """Initialize the tile"""
+        pass
+
+class Player(pygame.sprite.Sprite):
+    """A class the user can control"""
+
+    def __init__(self):
+        """Initialize the player"""
+        pass
+
+    def update(self):
+        """Update the player"""
+        pass
+
+    def move(self):
+        """Move the player"""
+
+    def check_collisions(self):
+        """Check for collisions with platforms and portals"""
+        pass
+
+    def check_animations(self):
+        """Check to see if the jump/fire animations should run"""
+        pass
+
+    def jump(self):
+        """Jump upwards if on a platform"""
+        pass
+
+    def fire(self):
+        """Fire a projectile from sword"""
+        pass
+
+    def reset(self):
+        """reset the player's position"""
+        pass
+
+    def animate(self):
+        """Animate the player's actions"""
+        pass
+
+class Projectile(pygame.sprite.Sprite):
+    """A projectile launched by the player"""
+
+    def __init__(self):
+        """Initialize the projectile"""
+        pass
+
+    def update(self):
+        """Update the projectile"""
+        pass
+
+class Zombie(pygame.sprite.Sprite):
+    """A class to create enemy zombies that move across the screen"""
+
+    def __init__(self):
+        """Initialize the zombie"""
+        pass
+
+    def update(self):
+        """Update the zombie"""
+        pass
+
+    def move(self):
+        """Move the zombie"""
+
+    def check_collisions(self):
+        """Check for collisions with platforms and portals"""
+        pass
+
+    def check_animations(self):
+        """Check to see if the death/rise animations should run"""
+        pass
+
+    def animate(self):
+        """Animate the zombie's actions"""
+        pass
+
+class RubyMaker(pygame.sprite.Sprite):
+    """A tile that is animated. A ruby will be generated here"""
+
+    def __init__(self):
+        """Initialize the ruby maker"""
+        pass
+
+    def update(self):
+        """Update the ruby maker"""
+        pass
+
+    def animate(self):
+        """Animate the ruby maker"""
+        pass
+
+class Ruby(pygame.sprite.Sprite):
+    """A class the player must collect to earn points and health"""
+
+    def __init__(self):
+        """Initialize the ruby"""
+        pass
+
+    def update(self):
+        """Update the ruby"""
+        pass
+
+    def move(self):
+        """Move the ruby"""
+        pass
+
+    def check_collisions(self):
+        """Check to see if the ruby has collided with platforms and portals"""
+        pass
+
+    def animate(self):
+        """Animate the ruby"""
+        pass
+
+class Portal(pygame.sprite.Sprite):
+    """A class to create portals that will transport you if you collide with it"""
+
+    def __init__(self):
+        """Initialize the portal"""
+        pass
+
+    def update(self):
+        """Update the portal"""
+        pass
+
+    def animate(self):
+        """Animate the portal"""
+        pass
+
+#Load and resize the background
+bg_image = pygame.transform.scale(pygame.image.load("./zombie_knight_assets/images/background.png"), (1280, 736)).convert_alpha()
+bg_rect = bg_image.get_rect(topleft = (0,0))
+
+#The main game loop
+running = True
+while running:
+    #Check to see if the user wants to quit
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    #Blit the background to the screen
+    display_surface.blit(bg_image, bg_rect)
+
+    #Update the display and tick the clock
+    pygame.display.update()
+    clock.tick(FPS)
+
+pygame.quit()
+sys.exit()
